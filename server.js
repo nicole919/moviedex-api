a9797d1581385fd390c764815f3c27e289354b14
Nicole Fafard <nicole.fafard@gmail.com>
2019-11-07 15:35:44 -0700

    added GET endpoint w search options

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const MOVIES = require("./movies-data.json");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-  res.send("Hello, world!");
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  next();
+});
+
+app.get("/movie", function handleGetGenre(req, res) {
+  let response = MOVIES;
+
+  if (req.query.genre) {
+    response = respones.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+  if (req.query.country) {
+    response = respones.filter(movie =>
+      movie.country.toLowerCase().includes(req.country.genre.toLowerCase())
+    );
+  }
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+  res.json(response);
 });
 
 const PORT = 8000;
